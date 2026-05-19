c2fa1a0744f1148841159f98a1f97122b4c59b6c
szd489 <szymon.desperak.3@gmail.com>
2026-05-19 10:43:31 +0200

    Create folder

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "a = ";
+    double a;
+    cin >> a;
+    cout << "b = ";
+    double b;
+    cin >> b;
+    cout << "Pole wynosi: " << a * b << endl;
+    return 0;
+}
